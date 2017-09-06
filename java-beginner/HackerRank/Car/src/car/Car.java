d75be8944af222d291b52bc81f414ffe8dadffba
ammarasmro <ammarasmaro@gmail.com>
2017-09-06 03:42:52 -0600

    Done car
    Done with challenge 2

--- java-beginner/HackerRank/Car/src/car/Car.java
+++ java-beginner/HackerRank/Car/src/car/Car.java
@@ -11,16 +11,41 @@ package car;
  */
 public class Car {
     
-    int maxSpeed;
-    int minSpeed;
-    double weight;
+    int maxSpeed = 100;
+    int minSpeed = 0;
+    double weight = 4079;
+    boolean isTheCarOn = false;
+    char condition = 'A';
+    String nameOfCar = "Lucy";
     
+    public void printVariables(){
+        System.out.println("This is the max speed " + maxSpeed);
+        System.out.println(minSpeed);
+        System.out.println(weight);
+        System.out.println(isTheCarOn);
+        System.out.println(condition);
+        System.out.println(nameOfCar);
+    }
+    
+    public void wreckCar(){
+        condition = 'C';
+    }
+    
+    public void upgradeMinSpeed(){
+        minSpeed = maxSpeed;
+        maxSpeed = maxSpeed + 1;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Car familyCar = new Car();
+        System.out.println("Family car: ");
+        familyCar.printVariables();
+        familyCar.upgradeMinSpeed();
+        familyCar.printVariables();
     }
     
 }
