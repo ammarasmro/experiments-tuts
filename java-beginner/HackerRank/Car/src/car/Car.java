c00b4559fd0d744e705795fd09d4ce776585e060
ammarasmro <ammarasmaro@gmail.com>
2017-09-06 03:32:00 -0600

    changed folder name
    Udacity to HackerRank

---
+++ java-beginner/HackerRank/Car/src/car/Car.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car;
+
+/**
+ *
+ * @author ammarasmro
+ */
+public class Car {
+    
+    int maxSpeed;
+    int minSpeed;
+    double weight;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
