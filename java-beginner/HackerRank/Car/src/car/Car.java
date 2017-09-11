6e895d166037eeeac6064ceeb5fabeef824ed55d
ammarasmro <ammarasmaro@gmail.com>
2017-09-10 18:31:33 -0600

    Hacker-rank-first
    Following the tutorial on HackerRank

--- java-beginner/HackerRank/Car/src/car/Car.java
+++ java-beginner/HackerRank/Car/src/car/Car.java
@@ -18,6 +18,18 @@ public class Car {
     char condition = 'A';
     String nameOfCar = "Lucy";
     
+    double maxFuel = 16;
+    double currentFuel = 8;
+    double mpg = 26.4;
+    
+    int numberOfPeopleInCar = 1;
+    
+    public Car(int customMaxSpeed, double customWeight, boolean customIsTheCarOn){
+        maxSpeed = customMaxSpeed;
+        weight = customWeight;
+        isTheCarOn = customIsTheCarOn;
+    }
+    
     public void printVariables(){
         System.out.println("This is the max speed " + maxSpeed);
         System.out.println(minSpeed);
@@ -25,6 +37,7 @@ public class Car {
         System.out.println(isTheCarOn);
         System.out.println(condition);
         System.out.println(nameOfCar);
+        System.out.println(numberOfPeopleInCar);
     }
     
     public void wreckCar(){
@@ -35,17 +48,41 @@ public class Car {
         minSpeed = maxSpeed;
         maxSpeed = maxSpeed + 1;
     }
+    
+    public void getIn(){
+        numberOfPeopleInCar++;
+    }
+    
+    public void getOut(){
+        numberOfPeopleInCar--;
+    }
+    
+    public double howManyMilesTillOutOfGas(){
+        return currentFuel * mpg;
+    }
+    
+    public double maxMilesPerFillUp(){
+        return maxFuel * mpg;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Car familyCar = new Car();
-        System.out.println("Family car: ");
-        familyCar.printVariables();
-        familyCar.upgradeMinSpeed();
-        familyCar.printVariables();
+        Car birthdayPresent = new Car(500, 5000.5, true);
+        System.out.println("Birthday car v1");
+        birthdayPresent.printVariables();
+        birthdayPresent.getIn();
+        birthdayPresent.getIn();
+        birthdayPresent.getIn();
+        System.out.println("Miles left: " + birthdayPresent.howManyMilesTillOutOfGas());
+        System.out.println("Max Miles: " + birthdayPresent.maxMilesPerFillUp());
+        System.out.println("Birthday car v2");
+        birthdayPresent.printVariables();
+        birthdayPresent.getOut();
+        System.out.println("Birthday present v3");
+        birthdayPresent.printVariables();
     }
     
 }
